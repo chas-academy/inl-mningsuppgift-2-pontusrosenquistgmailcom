b80268f61eda78cbc75ed2e01c89143fb0ba8514
pontus.rosenquist@gmail.com <pontus.rosenquist@gmail.com>
2025-03-18 20:52:31 +0100

    spelling fahrenheit is hard

--- main.c
+++ main.c
@@ -3,6 +3,16 @@
 
 // This is where you add the two functions
 
+float fahrenheit_to_celsius(float f){
+    return (f -32)*5/9;
+}
+
+
+float celsius_to_fahrenheit(float c){
+    return (c*9/5) +32;
+}
+
+
 int main() {
     float temp_f = 100.0;
     float temp_c = 37.0;
